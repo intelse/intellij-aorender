5a624ac65b6a1588320c156e4c014631b3c6248c
intelse <edu.intelse@gmail.com>
2022-11-01 15:10:36 -0300

    Criando Class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -12,5 +12,42 @@ public class PrimeiroPrograma {
         Gato cat = new Gato();
         System.out.println(cat);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
+
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPagin;
+
+    public Livro(String nome, Integer numPagin) {
+        this.nome = nome;
+        this.numPagin = numPagin;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagin() {
+        return numPagin;
+    }
+
+    public void setNumPagin(Integer numPagin) {
+        this.numPagin = numPagin;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagin=" + numPagin +
+                '}';
     }
 }
