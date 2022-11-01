0ef816455b63ffbbf0cc18b68576559592f42462
intelse <edu.intelse@gmail.com>
2022-11-01 14:58:33 -0300

    primeiro de muitos

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 2;
+
+        System.out.println("Hello world " + (a+b));*/
+
+        Gato cat = new Gato();
+        System.out.println(cat);
+
+    }
+}
